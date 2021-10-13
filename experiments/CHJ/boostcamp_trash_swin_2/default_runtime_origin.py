bc742f3343278691a7fd061f4374468d315e60cc
gkswns3708 <gkswns3708@naver.com>
2021-10-13 08:01:03 +0000

    CHJ test commit

---
+++ experiments/CHJ/boostcamp_trash_swin_2/default_runtime_origin.py
@@ -0,0 +1,24 @@
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        # dict(type='TextLoggerHook'),
+        dict(type='KSTTextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='swin transformer',
+                name='CHJ'
+            )
+        )
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
