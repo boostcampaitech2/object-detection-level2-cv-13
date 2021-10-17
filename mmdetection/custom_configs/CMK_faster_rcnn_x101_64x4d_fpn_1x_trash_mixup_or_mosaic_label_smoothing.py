52ef26c73af86af1406b157693f311e7d4045498
note823 <adc1231000@naver.com>
2021-10-17 14:47:13 +0900

    add config files

---
+++ mmdetection/custom_configs/CMK_faster_rcnn_x101_64x4d_fpn_1x_trash_mixup_or_mosaic_label_smoothing.py
@@ -0,0 +1,141 @@
+
+# ref: /opt/ml/detection/mmdetection/configs/yolox/yolox_s_8x8_300e_coco.py
+# from mmdet.custom.new_mosaic import NewMosaic
+_base_ = './faster_rcnn_r50_fpn_1x_coco.py'
+model = dict(
+    backbone=dict(
+        type='ResNeXt',
+        depth=101,
+        groups=64,
+        base_width=4,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        style='pytorch',
+        init_cfg=dict(
+            type='Pretrained', checkpoint='open-mmlab://resnext101_64x4d')),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='LabelSmoothingCrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=10,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='LabelSmoothingCrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),        
+    )
+
+# custom_imports = dict(imports=['mmdet.custom.mixup'], allow_failed_imports=False)
+
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/opt/ml/detection/dataset/'
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='MixUpOrMosaic', img_scale=(1024, 1024), center_ratio_range=(1.0, 1.0), ratio_range=(1.0,1.0), pad_val=0),
+    dict(type='Resize', img_scale=(1024, 900), keep_ratio=True),
+    # dict(type='Resize', img_scale=(900, 900), keep_ratio=True),
+    # dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+
+train_dataset = dict(
+    type='MultiImageMixDataset',
+    dataset=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train.json',
+        img_prefix=data_root,
+        pipeline=[
+            dict(type='LoadImageFromFile', to_float32=True),
+            dict(type='LoadAnnotations', with_bbox=True)
+        ],
+        # filter_empty_gt=False,
+    ),
+    pipeline=train_pipeline,
+    # dynamic_scale=(1024, 900)
+)
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 900),
+        flip=False,
+        transforms=[
+            # dict(type='Resize', keep_ratio=True),
+            dict(type='Resize', img_scale=(1024, 900), keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    _delete_=True,
+    samples_per_gpu=6,
+    workers_per_gpu=4,
+    train=train_dataset,
+    val=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root + 'val.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline),
+    test=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+runner = dict(type='EpochBasedRunner', max_epochs=12)
+
+load_from = "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco/faster_rcnn_x101_64x4d_fpn_1x_coco_20200204-833ee192.pth"
--- mmdetection/custom_configs/CMK_faster_rcnn_x101_64x4d_fpn_1x_trash_mosaic.py
+++ mmdetection/custom_configs/CMK_faster_rcnn_x101_64x4d_fpn_1x_trash_mosaic.py
@@ -1,4 +1,7 @@
-_base_ = '../configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+
+# ref: /opt/ml/detection/mmdetection/configs/yolox/yolox_s_8x8_300e_coco.py
+# from mmdet.custom.new_mosaic import NewMosaic
+_base_ = './faster_rcnn_r50_fpn_1x_coco.py'
 model = dict(
     backbone=dict(
         type='ResNeXt',
@@ -35,6 +38,8 @@ model = dict(
             loss_bbox=dict(type='L1Loss', loss_weight=1.0))),        
     )
 
+# custom_imports = dict(imports=['...mmdet.custom.mixup'], allow_failed_imports=False)
+
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = '/opt/ml/detection/dataset/'
@@ -43,15 +48,34 @@ classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass",
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='NewMosaic', img_scale=(1024, 1024), center_ratio_range=(1.0, 1.0), pad_val=0),
     dict(type='Resize', img_scale=(1024, 900), keep_ratio=True),
+    # dict(type='Resize', img_scale=(900, 900), keep_ratio=True),
+    # dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
+
+train_dataset = dict(
+    type='MultiImageMixDataset',
+    dataset=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train.json',
+        img_prefix=data_root,
+        pipeline=[
+            dict(type='LoadImageFromFile', to_float32=True),
+            dict(type='LoadAnnotations', with_bbox=True)
+        ],
+        # filter_empty_gt=False,
+    ),
+    pipeline=train_pipeline,
+    # dynamic_scale=(1024, 900)
+)
+
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -68,26 +92,34 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
+
 data = dict(
-    samples_per_gpu=8,
+    _delete_=True,
+    samples_per_gpu=6,
     workers_per_gpu=4,
-    train=dict(
-        type=dataset_type,
-        classes=classes,
-        ann_file=data_root + 'train.json',
-        img_prefix=data_root,
-        pipeline=train_pipeline),
+    train=train_dataset,
     val=dict(
-        type=dataset_type,
         classes=classes,
+        type=dataset_type,
         ann_file=data_root + 'val.json',
         img_prefix=data_root,
         pipeline=test_pipeline),
     test=dict(
-        type=dataset_type,
         classes=classes,
+        type=dataset_type,
         ann_file=data_root + 'test.json',
         img_prefix=data_root,
         pipeline=test_pipeline))
+
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+runner = dict(type='EpochBasedRunner', max_epochs=12)
 
 load_from = "https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_x101_64x4d_fpn_1x_coco/faster_rcnn_x101_64x4d_fpn_1x_coco_20200204-833ee192.pth"
